5ff0c0361b11b4137190b76575314dd9d5cce0a0
Robin Tweedie <phorque@gmail.com>
2011-02-06 14:05:09 +0000

    escape the selected text

--- songkicklet_min.js
+++ songkicklet_min.js
@@ -1,2 +1,2 @@
 
-(function(){var base_url="http://www.songkick.com/",selected_text=window.getSelection().toString(),url=base_url+"concerts/new?headliners[0][name]="+selected_text;window.open(url);}());
+(function(){var base_url="http://www.songkick.com/",selected_text=escape(window.getSelection().toString()),url=base_url+"concerts/new?headliners[0][name]="+selected_text;window.open(url);}());
