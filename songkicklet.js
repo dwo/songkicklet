d83b6517649ccad2693fffab51006643a7b59da9
Robin Tweedie <robin@songkick.com>
2012-04-27 00:02:18 +0100

    update the songkicklet

--- songkicklet.js
+++ songkicklet.js
@@ -2,6 +2,6 @@
 (function () {
   var base_url = "http://www.songkick.com/",
   selected_text = escape(window.getSelection().toString()),
-  url = base_url + "concerts/new?headliners[0][name]=" + selected_text;
+  url = base_url + "concerts/new?event[headliner_names][0]=" + selected_text;
   window.open(url);
 }());
