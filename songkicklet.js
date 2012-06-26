bbd350d01d1c8a45758b9ae76d01be1b41923ddd
Robin Tweedie <robin@songkick.com>
2012-06-26 14:14:02 +0100

    derp

--- songkicklet.js
+++ songkicklet.js
@@ -1,7 +1,7 @@
 /*jslint browser: true, onevar: true, undef: true, newcap: true, nomen: true, regexp: true, plusplus: true, bitwise: true, maxerr: 50, maxlen: 80, indent: 2 */
 (function (window) {
   var base_url = "http://www.songkick.com/",
-  selected_text = escapeURIComponent(window.getSelection().toString()),
+  selected_text = encodeURIComponent(window.getSelection().toString()),
   url = base_url + "concerts/new?event[headliner_names][0]=" + selected_text;
   window.open(url);
 }());
